0dc193df5d24b4fce9b01446fc954f7cd341a3cb
JhonierRojas <yonier202@gmail.com>
2024-02-21 09:01:15 -0500

    eventos para llenar el objeto con sus atributos

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
-const resultado = document.querySelector('#resultado');
+const marca = document.querySelector('#marca');
 const year = document.querySelector('#year');
+const minimo = document.querySelector('#minimo');
+const maximo = document.querySelector('#maximo');
+const puertas = document.querySelector('#puertas');
+const transmision = document.querySelector('#transmision');
+const color = document.querySelector('#color');
+
+
+const resultado = document.querySelector('#resultado');
 
 const max= new Date().getFullYear();
 const min = max - 10;
@@ -7,14 +15,48 @@ const min = max - 10;
 // console.log(max);
 // console.log(min);
 
+//Generar Objeto con la busuqeda
+const datosBusqueda = {
+    marca: "",
+    year: "",
+    minimo: "",
+    maximo: "",
+    puertas: "",
+    transmision: "",
+    color: ""
+}
 
+//eventos
 document.addEventListener("DOMContentLoaded", ()=> {
     mostrarAutos();
 
     //llena las opciones de años
 
     llenarSelect();
-});  
+}); 
+
+marca.addEventListener("change", (e)=> {
+    datosBusqueda.marca = e.target.value;
+});
+year.addEventListener("change", (e)=> {
+    datosBusqueda.year = e.target.value;
+});
+minimo.addEventListener("change", (e)=> {
+    datosBusqueda.minimo = e.target.value;
+});
+maximo.addEventListener("change", (e)=> {
+    datosBusqueda.maximo = e.target.value;
+});
+puertas.addEventListener("change", (e)=> {
+    datosBusqueda.puertas = e.target.value;
+});
+transmision.addEventListener("change", (e)=> {
+    datosBusqueda.transmision = e.target.value;
+});
+color.addEventListener("change", (e)=> {
+    datosBusqueda.color = e.target.value;
+    console.log(datosBusqueda);
+});
 
 // funciones 
 function mostrarAutos() {
